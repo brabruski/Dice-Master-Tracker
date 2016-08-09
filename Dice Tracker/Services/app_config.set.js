d117ef49cf2279c2a127d8bed210e7881324a6cb
Glen Miller <brabruski@hotmail.com>
2016-08-09 15:23:41 +0200

    Added ngMaterial Buttons

--- Dice Tracker/Services/app_config.set.js
+++ Dice Tracker/Services/app_config.set.js
@@ -3,8 +3,22 @@
     FIREBASE_IMG_URL: 'gs://dicetracker18052016.appspot.com/'
 });
 
+//Define Theme defaults
 logApp.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
       .primaryPalette('pink')
       .accentPalette('orange');
+});
+
+//Define icons in theme
+logApp.config(function ($mdIconProvider) {
+    // Configure URLs for icons specified by [set:]id.
+    $mdIconProvider
+        .defaultFontSet('fa')                   // This sets our default fontset className.
+        .icon('delete', '../images/icons/delete.svg')
+        .icon('add', '../images/icons/plus-circle.svg')
+        .icon('show', '../images/icons/spotlight-beam.svg')
+        .icon('aleft', '../images/icons/chevron-double-left.svg')
+        .icon('aright', '../images/icons/chevron-double-right.svg')
+        .icon('back', '../images/icons/backburger.svg')
 });

fc4dd4a6638f2b7f9b9bfa9f2deeca1a01f69afc
Glen Miller <brabruski@hotmail.com>
2016-08-18 14:11:45 +0200

    Setting up to move methods into a factory

---
+++ Dice Tracker/Services/cardcollection.fac.js
@@ -0,0 +1,31 @@
+﻿logApp.factory('CollectionFactory',
+    ['$rootScope', '$firebaseAuth', '$firebaseObject', 'Config',
+    function ($rootScope, $firebaseAuth, $firebaseObject, Config) {
+        var cmObj = {
+            //Controller Working From Currently: carddetails.ctr.js
+            getRarity: function (item) {
+                var rarity = ["common", "uncommon", "rare", "srare"]
+                switch (item.rarity) {
+                    case "Uncommon":
+                        return rarity[1];
+                    case "Rare":
+                        return rarity[2];
+                    case "Super Rare":
+                        return rarity[3];
+                    default:
+                        return rarity[0];
+                }
+            },
+
+            hideActionVersion: function (typeOfCard) {
+                if (typeOfCard === 'Action') {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+
+        }
+        return cmObj;
+    }]);
